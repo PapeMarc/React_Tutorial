3dc8683543d8abf8c79ddac1e338f175e0feb5d0
PapeMarc <marc.pape@pape-development.com>
2024-07-29 14:22:14 +0200

    JSC / TSC in JSX / TSX

--- tutorial-app/src/Comp1.tsx
+++ tutorial-app/src/Comp1.tsx
@@ -1,9 +1,23 @@
 import App from "./App";
 
 export default function Comp1(){
+    const msg = "Some Message.";
+
     return (
-        <div>
-            <App></App>
+        <div id="comp1div">
+            <div id="msgdiv" style={{ fontSize: "40px", fontWeight: "bold", color: "lightgreen" }}>
+                {msg}
+            </div> 
+            {/*Alles in Klammern ist script*/}
+            <div>
+                {/*Other terms.*/ 1+2 + Math.PI}
+                <br/>
+                <br/>
+                <button onClick={() => {
+                    alert("It clicked me!");
+                }}>Click me!</button>
+            </div>
+            {/* <App></App> */}
         </div>
     );
 };
